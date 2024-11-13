21bdff1574f4f7153cd54a37985268ffff7616b7
EddSanchezz <edwarde.sanchezm@uqvirtual.edu.co>
2024-11-13 17:40:06 -0500

    update app.py

--- buscador/app.py
+++ buscador/app.py
@@ -1,8 +1,9 @@
 from buscador.buscador_funciones import extraer_datos, guardar_cookies, obtener_mejor_producto, agregar_a_whislist
 from buscador.opciones_driver import iniciar_chrome
-
+from buscador.utilidades import guardar_articulos_csv
 
 articulo = obtener_mejor_producto("carro rojo")
 #guardar_cookies()
 agregar_a_whislist(articulo)
 
+guardar_articulos_csv("rating.csv",articulo)
